8ed2fc432a4752f5882bff68ce23826149af249a
yinnping <ciaoing@livemail.tw>
2018-08-10 16:35:34 +0800

    It can get sub-folder name from Onedrive now.

--- quickstart.py
+++ quickstart.py
@@ -7,9 +7,8 @@ import json
 
 scopes = "https://www.googleapis.com/auth/drive"
 functionURL = r"https://prod-22.eastasia.logic.azure.com:443/workflows/f18d6c9e410b401e851a2fbdd10aaabb/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=UmjvwpNzBFkWMZW2pJh7CiCNE6Mj0vV-HtIkWSVgkZo"
-folderNames = []
 
-def CreateFolder(name, parents, service):
+def CreateFolder(folderObj, service):
     fileMetadata = {
         'name': name,
         'mimeType': 'application/vnd.google-apps.folder',
@@ -19,25 +18,32 @@ def CreateFolder(name, parents, service):
     newFile = service.files().create(body=fileMetadata, fields='id').execute()
     print('Folder ID:', newFile.get('id')) 
 
-def MakeRequest(folderName):
+def GetFoldersRequest(folderName):
     global functionURL
-    data = {"folder": folderName}
-    print(data)
+
+    rNames = []
+    data = {"folder": folderName}    
+    dataJson = json.dumps(data, ensure_ascii=False)    
     header = {"content-type": "application/json"}
-    response = requests.post(functionURL, data=json.dumps(data, ensure_ascii=False), headers=header)
-    print(response.status_code)
-    print(response.text)
+    response = requests.post(functionURL, data=dataJson.encode('utf-8'), headers=header)
+    
+    # if response.status_code != requests.codes.ok:        
+    #     return False        
+    # else:
+    return response.text, response.status_code
+
 
 def main():
     # Apply a token thought Oauth.
-    # store = file.Storage('token.json')
-    # credit = store.get()
-    # if not credit or credit.invalid:
-    #     flow = client.flow_from_clientsecrets('credentials.json', scopes)
-    #     credit = tools.run_flow(flow, store)    
-    # service = build('drive', 'v3', http = credit.authorize(Http()))    
+    store = file.Storage('token.json')
+    credit = store.get()
+    if not credit or credit.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', scopes)
+        credit = tools.run_flow(flow, store)    
+    service = build('drive', 'v3', http = credit.authorize(Http()))
 
-    MakeRequest('tet')
+    folderJob = GetFoldersRequest("11th MSP")
+    print(folderJob)
 
 if __name__ == '__main__':
     main()
